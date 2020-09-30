cca9167fadda6528e72c243d0a0835ab729a76b7
410911316 <71741598+410911316@users.noreply.github.com>
2020-09-30 15:25:03 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+let ＧＰＡ = 0
+input.onGesture(Gesture.Shake, function () {
+    ＧＰＡ = randint(80, 100)
+    basic.showNumber(ＧＰＡ)
+    if (ＧＰＡ < 80) {
+        basic.showString("B+")
+    } else if (ＧＰＡ < 85) {
+        basic.showString("A-")
+    } else if (ＧＰＡ < 90) {
+        basic.showString("A")
+    } else if (ＧＰＡ <= 100) {
+        basic.showString("A+")
+    }
+})
 basic.forever(function () {
 	
 })
